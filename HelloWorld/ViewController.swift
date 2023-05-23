aa224c2ca0bfbb584ba6e79894396b747f9faa20
Artem Potemkin (M1 Pro) <art.potemkin@gmail.com>
2023-05-23 14:27:58 +0300

    add 2 views

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -1,16 +1,8 @@
 //
 //  ViewController.swift
 //  HelloWorld
-// abra-kadabra
-// test commit from newMacbook
-// someChanges for test to Remote 
 //  Created by Артём Потёмкин on 19.05.2023.
-// add text from gitHub
-// local commit from Mac M1
-// 18: 41
-// 
-// two strings for push..
-// two strings from 18:48 from new MacBook
+
 
 import UIKit
 
